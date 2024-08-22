26095054d7d738679ee6e65c0e2dd7cc6c0f6eb9
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-08-22 16:33:43 +0300

    Application Base Setup #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,66 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer'); 
+
 module.exports = {
-    mode: "development",
-    entry: "./src/test.tsx",
+    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    entry: {
+        popup: path.resolve('./src/popup/popup.tsx'),
+        options: path.resolve('./src/options/options.tsx'),
+    },
     module: {
         rules: [
             {
-                use: "ts-loader",
+                use: 'ts-loader',
                 test: /\.tsx$/,
                 exclude: /node_modules/,
-            }
+            },
+            {
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader', // postcss loader needed for tailwindcss
+                    options: {
+                        postcssOptions: {
+                            ident: 'postcss',
+                            plugins: [tailwindcss, autoprefixer],
+                        }
+                    }
+                }],
+                test: /\.css$/i,
+            },
         ]
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+              { 
+                from: path.resolve('src/static'),
+                to: path.resolve('dist') 
+              },
+              
+              
+            ],
+        }),
+        ...getHtmlPlugins([
+            'popup',
+            'options',
+            'newTab'
+        ]),
+    ],
     resolve: {
-        extentions: ['.tsx', '.ts', '.js']
+        extensions: ['.tsx', '.ts', '.js']
     },
     output: {
-        filename: "index.tsx",
+        filename: '[name].js',
     }
+}
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(chunk => new HtmlWebpackPlugin({
+        title: 'LeetCrush Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk]
+    }))
 }
